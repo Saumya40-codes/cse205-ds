8029ce53a9dc7e0446450b158bed9e4f615751a0
Saumya Shah <115284013+Saumya40-codes@users.noreply.github.com>
2023-08-21 14:59:17 +0530

    subsets 2

--- assignments/16.08.2023/90.java
+++ assignments/16.08.2023/90.java
@@ -1,30 +1,23 @@
 class Solution {
 
-    private static void perms(int[] arr, List<Integer> lst, List<List<Integer>> ans){
-        if(lst.size() == arr.length){
-            ans.add(new ArrayList<>(lst));
+    private static void subseq(int[] nums, int idx, List<Integer> lst , List<List<Integer>>  ans){
 
+        if( idx >= nums.length){
+            if(!(ans.contains(new ArrayList<>(lst))))
+                ans.add(new ArrayList<>(lst));
             return;
         }
 
-
-        for(int val: arr){
-            if(!lst.contains(val)){
-                lst.add(val);
-                perms(arr, lst, ans);
-                lst.remove(lst.size()-1);
-            }
-        }
-        
+            lst.add(nums[idx]);
+            subseq(nums,idx+1,lst,ans);
+            lst.remove(lst.size()-1);
+            subseq(nums,idx+1,lst,ans);
     }
-
-
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>>  ans = new ArrayList<>();
         List<Integer> lst = new ArrayList<>();
-        List<List<Integer>> ans = new ArrayList<>();
-
-        perms(nums, lst, ans);
-
+        Arrays.sort(nums);
+        subseq(nums, 0, lst, ans);
         return ans;
     }
 }
